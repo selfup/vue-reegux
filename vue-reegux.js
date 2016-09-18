084f690d9c0e72a835d34cbe7e982fa49fd927d9
Regis <boudinot.regis@yahoo.com>
2016-09-18 17:45:37 -0600

    update

--- vue-reegux.js
+++ vue-reegux.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -16,12 +16,17 @@ window.VueReegux = {
       }
 
       _createClass(_class, [{
-        key: "send",
+        key: 'send',
         value: function send(message, arg) {
-          this.actions[message](this.state, arg);
+          var newState = this.actions[message](this.state, arg);
+          if (newState === this.state) {
+            console.error('please return a copy/clone');
+            return 'please return a copy/clone';
+          }
+          this.state = newState;
         }
       }, {
-        key: "install",
+        key: 'install',
         value: function install(Vue, options) {
           Vue.$rgx = this;
           Vue.addVueReeguxController = function (app) {
